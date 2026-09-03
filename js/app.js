017110e95b5f2199fac7de8a824d18ec52e34c1a
Daniel <dancode99@gmail.com>
2026-09-02 23:27:59 -0400

    Refactor: organizar páginas y modularizar JS con Header y Footer dinámico

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
- // 1. JSON simulado con los datos de los productos
+import { renderHeader } from "./components/header.js";
+import { renderFooter } from "./components/footer.js";
 import { datosProductos } from "./productos/productos.js";
+
+// Renderizar componentes compartidos
+renderHeader();
+renderFooter();
 
 // 2. Capturar el contenedor usando su ID (ya no necesitamos el [0])
 const contenedor = document.getElementById("contenedor-productos");
---
+++ js/productos.js
@@ -0,0 +1,7 @@
+import { renderHeader } from "./components/header.js";
+import { renderFooter } from "./components/footer.js";
+
+renderHeader();
+renderFooter();
+
+console.log("Página de productos cargada correctamente.");
